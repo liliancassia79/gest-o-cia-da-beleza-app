bc3d63ef3e1c67745502f108ba319547a3710386
Lilian Sousa <liliancassia79@gmail.com>
2026-05-20 08:47:30 -0300

    chore: adicionando configuracao do netlify para o tanstack start

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import netlifyVitePlugin from "@netlify/vite-plugin-tanstack-start";
 import path from "path";
 
 export default defineConfig({
@@ -32,6 +33,7 @@ export default defineConfig({
         }
       }
     }),
+    netlifyVitePlugin(),
     viteReact(),
   ],
   server: {
